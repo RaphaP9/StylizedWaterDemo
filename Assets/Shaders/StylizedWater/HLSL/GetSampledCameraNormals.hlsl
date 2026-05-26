6e04dfdb03b183dee1d35ceaec5797ab7260b88f
Raphael <115659111+RaphaP9@users.noreply.github.com>
2026-05-26 10:30:43 -0500

    Initial Commit

---
+++ Assets/Shaders/StylizedWater/HLSL/GetSampledCameraNormals.hlsl
@@ -0,0 +1,13 @@
+﻿#ifndef SHADERGRAPH_PREVIEW
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareNormalsTexture.hlsl"
+#endif
+
+void GetSampledCameraNormals_float(float2 UV, out float3 SampledCameraNormals)
+{
+#ifdef SHADERGRAPH_PREVIEW
+    SampledCameraNormals =  float3(0.0, 0.0, 1.0);
+#else
+    float3 sampledNormals = SampleSceneNormals(UV);
+    SampledCameraNormals = TransformWorldToViewDir(sampledNormals);
+#endif   
+}
